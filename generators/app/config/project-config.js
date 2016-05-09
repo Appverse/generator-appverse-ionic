7be7c91e90f87e95ba267b1604e1300cb8473cdd
Pau Martinez <pau.martinez@gft.com>
2016-05-09 15:36:57 +0200

    fix template service

--- generators/app/config/project-config.js
+++ generators/app/config/project-config.js
@@ -5,31 +5,31 @@ module.exports = {
         "app/index.html",
         "package.json",
         "bower.json",
-        "Gruntfile.js", 
+        "Gruntfile.js",
         "README.md",
-        "sonar-project.properties", 
+        "sonar-project.properties",
         "app/states/app-states.js",
         "app/app.js",
         "test/e2e/homeSpec.js",
-        "tasks/mobile.js" 
+        "tasks/mobile.js",
+        "app/directives"
     ],
-   files: [ 
-        ".bowerrc",    
+   files: [
+        ".bowerrc",
         ".editorconfig",
         ".jshintrc",
         ".jscsrc",
         ".jshintignore",
-        ".jsbeautifyrc",   
+        ".jsbeautifyrc",
         "LICENSE.md",
         "app/favicon.ico",
-        "app/components", 
-        "app/directives",
-        "app/js",  
+        "app/components",
+        "app/js",
         "app/styles",
         "config",
         "mobile-builder-bundle",
         "tasks",
         "test",
         "app/js"
-    ] 
+    ]
 }
